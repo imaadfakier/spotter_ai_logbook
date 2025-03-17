1cf0413146e1d60d6e2f1f134556f9719bee6b74
Imaad Fakier <79690509+imaadfakier@users.noreply.github.com>
2025-03-17 22:03:50 +0200

    Update settings.py

--- spotter_ai_logbook/spotter_ai_trucker_logbook/settings.py
+++ spotter_ai_logbook/spotter_ai_trucker_logbook/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["*"]  # os.environ.get('ALLOWED_HOSTS').split(",")
+ALLOWED_HOSTS = ["imaadfakier.pythonanywhere.com"]
 
 # Application definition
 
