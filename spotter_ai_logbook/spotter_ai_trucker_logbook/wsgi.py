4580598dd918729ef93dd84c883c04626729004d
imaadfakier <ttkhastalamuerte@gmail.com>
2025-03-18 00:27:43 +0200

    fix: explicitly add the project to sys.path

--- spotter_ai_logbook/spotter_ai_trucker_logbook/wsgi.py
+++ spotter_ai_logbook/spotter_ai_trucker_logbook/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotter_ai_trucker_logbook.settings')
+# Add the outer directory to sys.path so the app can find modules in the nested folder
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spotter_ai_trucker_logbook.settings")
 
 application = get_wsgi_application()
