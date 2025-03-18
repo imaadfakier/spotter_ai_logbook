ce453790ff3711105e39eb7b17aa64fc41802e7e
imaadfakier <ttkhastalamuerte@gmail.com>
2025-03-18 07:36:45 +0200

    fix: remove STATICFILES_DIRS setting to resolve deployment warning

--- spotter_ai_trucker_logbook/settings.py
+++ spotter_ai_trucker_logbook/settings.py
@@ -126,11 +126,6 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# Serve static files during deployment
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
